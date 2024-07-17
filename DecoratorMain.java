cec16d949730ef457f382db43cc3d244ee6a8886
Carl Angelo Cabahug <ca.cabahug@gmail.com>
2024-07-17 11:39:30 +0800

    DecMain add

--- DecoratorMain.java
+++ DecoratorMain.java
@@ -7,12 +7,47 @@
 class DecoratorMain
 {
     public static void main(String[] args)
-	{	
+	{
+
+        System.out.println("""
+                    
+                    
+                       ,---.-,                                                                   \s
+                      '   ,'  '.                                                                 \s
+                     /   /      \\                                                       ,---,    \s
+                    .   ;  ,/.  :    ,---,.  ,---,    ,---,.  ,---,    ,---,.  ,---,  .'  .' `\\  \s
+                    '   |  | :  ;  ,'  .' |,'  .'|  ,'  .' |,'  .'|  ,'  .' |,'  .'|,---.'     \\ \s
+                    '   |  ./   :,---.'  ,---.'  |,---.'  ,---.'  |,---.'  ,---.'  ||   |  .`\\  |\s
+                    |   :       ,|   |   |   |   ;|   |   |   |   ;|   |   |   |   ;:   : |  '  |\s
+                     \\   \\     / :   :  .:   :  .':   :  .:   :  .':   :  .:   :  .'|   ' '  ;  :\s
+                      ;   ,   '\\ :   |.' :   |.'  :   |.' :   |.'  :   |.' :   |.'  '   | ;  .  |\s
+                     /   /      \\`---'   `---'    `---'   `---'    `---'   `---'    |   | :  |  '\s
+                    .   ;  ,/.  :                                                   '   : | /  ; \s
+                    '   |  | :  ;                                                   |   | '` ,/  \s
+                    '   |  ./   :                                                   ;   :  .'    \s
+                    |   :      /                                                    |   ,.'      \s
+                     \\   \\   .'                                                     '---'        \s
+                      `---`-'                                                                    \s
+                    
+                    
+                    """);
+
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+        System.out.println("""
+                
+                    ___  __  ___   ___  __    _   _____  ___  __ \s
+                   /   \\/__\\/ __\\ /___\\/__\\  /_\\ /__   \\/___\\/__\\\s
+                  / /\\ /_\\ / /   //  // \\// //_\\\\  / /\\//  // \\//\s
+                 / /_///__/ /___/ \\_// _  \\/  _  \\/ / / \\_// _  \\\s
+                /___,'\\__/\\____/\\___/\\/ \\_/\\_/ \\_/\\/  \\___/\\/ \\_/\s
+                                                                 \s
+                """);
         try
         {
             // Creates a SystemPlan object that is wrapped around each other to form a chain of decoratrs. Each decorator class adds its own behavior to the system plan.
 
-            System.out.println("\n\nOBJECT 1:\n");
+            System.out.println("OBJECT 1:\n");
             SystemPlan c1 = new EquipmentSetup(new VloggingSetup(new StreamingSetup(new AudioSetup())));
             // Shows the total cost of the system
             System.out.println("Total cost of the system is " + c1.getSystemCost());
@@ -23,6 +58,10 @@ class DecoratorMain
             SystemPlan b1 = new EquipmentSetup(new VloggingSetup(new StreamingSetup(new VoiceSetup())));
             System.out.println("Total cost of the system is " + b1.getSystemCost());
             b1.getSystemDetails();
+
+
+
+
         }
         catch(Exception e){
             System.out.println("whatsup");
